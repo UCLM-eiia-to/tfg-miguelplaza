423eb7caa24ad79b6cd9afef748a3312f3f6a853
MiguelPlaza99 <129739178+MiguelPlaza99@users.noreply.github.com>
2023-04-20 16:48:41 +0200

    Add files via upload

---
+++ src/PS4-esp32-master/examples/i2c_master_pwm/i2c_master_pwm.ino
@@ -0,0 +1,44 @@
+/*
+  I2C Master Control Demo
+  i2c-master-demo-control.ino
+  Demonstrate use of I2C bus
+  Master sends potentimeter position data
+  DroneBot Workshop 2019
+  https://dronebotworkshop.com
+*/
+
+// Include Arduino Wire library for I2C
+#include <Wire.h>
+
+// Define Slave I2C Address
+#define SLAVE_ADDR 9
+
+// Analog pin for potentiometer
+const int analogPin = 34;
+// Integer to hold potentiometer value
+int val = 0;
+
+void setup() {
+
+  // Initialize I2C communications as Master
+  Wire.begin();
+  Serial.begin(115200);
+
+
+}
+
+void loop() {
+  delay(50);
+
+  // Read pot value
+  // Map to range of 1-255 for flash rate
+  val = map(analogRead(analogPin), 0, 4095, 1, 255);
+
+  // Write a charatre to the Slave
+  Wire.beginTransmission(SLAVE_ADDR);
+  Wire.write(val);
+  Wire.endTransmission();
+  Serial.println(val);
+
+
+}
